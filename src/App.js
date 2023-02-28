0633230b73485bcb925572e6966767ffa7265e53
Peter <peterayuba75@yahoo.com>
2023-02-28 23:56:57 +0100

    Ship project MVP

---
+++ src/App.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import "./App.scss"
+import { newKitFromWeb3 } from '@celo/contractkit'
+import BigNumber from "bignumber.js";
+import Web3 from 'web3';
+import abi from "./contracts/todo.abi.json"
+
+const ERC20_DECIMALS = 18
+const contractAddress = "0x8216E0B0B9b7ddcD36e22A23e3B33fE1b9409685";
+
+const App = () => {
+  const [address, setAddress] = React.useState()
+  const [balance, setBalance] = React.useState()
+  const [notes, setNotes] = React.useState()
+  const [title, setTitle] = React.useState()
+  const [note, setNote] = React.useState();
+  const [kit, setKit] = React.useState();
+  const [todoContract, setTodoContract] = React.useState()
+
+
+  const accountBalance = async () => {
+    try {
+      const balance = await kit.getTotalBalance(String(address));
+      const celoBalance = balance.CELO
+      const _todoContract = new kit.web3.eth.Contract(abi, contractAddress);
+
+      setTodoContract(_todoContract)
+      setBalance(celoBalance)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const connectWallet = async () => {
+    if (window.celo) {
+      try {
+        await window.celo.enable();
+        const web3 = new Web3(window.celo);
+        let kit = newKitFromWeb3(web3)
+
+        const account = await kit.web3.eth.getAccounts();
+        const acc = account[0];
+        kit.defaultAccount = acc;
+        setKit(kit)
+        setAddress(account.toString())
+      } catch (e) {
+        console.log(e)
+      }
+    } else {
+      alert("Please install CeloExtensionWallet to continue with this app")
+    }
+  }
+
+  React.useEffect(() => {
+    connectWallet()
+  }, [])
+
+  React.useEffect(() => {
+    if (kit && address) {
+      accountBalance()
+    }
+  }, [kit, address])
+
+  React.useEffect(() => {
+    if (todoContract) {
+      fetchNotes()
+    }
+  }, [todoContract])
+
+  const fetchNotes = async () => {
+    try {
+      const rawNotes = await todoContract.methods.getNotes().call();
+      const cleanNotes = await Promise.all(
+        rawNotes.map(note => {
+          return {
+            id: note.noteId,
+            title: note.title,
+            note: note.note,
+            completed: note.completed,
+            lastModified: note.lastModified,
+            tag: note.tag
+          }
+        })
+      )
+      console.log(cleanNotes)
+      setNotes(cleanNotes)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const add = async () => {
+    try {
+      await todoContract.methods.addNote(
+        title,
+        note,
+        "public"
+      ).send({ from: kit.defaultAccount })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const complete = async (noteId) => {
+    try {
+      await todoContract.methods.setCompleted(noteId).send({ from: kit.defaultAccount })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const deleteNote = async (noteId) => {
+    try {
+      await todoContract.methods.deleteNote(noteId).send({ from: kit.defaultAccount })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  // format a wallet address
+  const truncateAddress = (address) => {
+    if (!address) return
+    return address.slice(0, 5) + "..." + address.slice(address.length - 4, address.length);
+  }
+
+  // convert from big number
+  const formatBigNumber = (num) => {
+    if (!num) return
+    return num.shiftedBy(-ERC20_DECIMALS).toFixed(2);
+  }
+  return (
+    <>
+      <div className='main'>
+        <div className='nav'>
+          <div className='address'>{truncateAddress(address)}</div>
+          <div className='bal'>{formatBigNumber(BigNumber(balance))} CELO</div>
+        </div>
+        <div className='todo'>
+          <div className='add'>
+            <input onChange={e => setTitle(e.target.value)} value={title} placeholder="Enter title here" />
+            <textarea onChange={e => setNote(e.target.value)} value={note} placeholder="Enter note here" />
+            <button onClick={add}>Add Note</button>
+          </div>
+          {notes?.map(note => (
+            <div className='note'>
+              <div className='header'>
+                <div className='title'>{note.title}</div>
+                <div className='headerR'>
+                  <div className='completed'>{note.completed ? "Completed" : "Active"}</div>
+                  <div className='lastModified'>{new Date(note.lastModified * 1000).toLocaleTimeString()}</div>
+                </div>
+              </div>
+              <hr />
+              <div className='body'>{note.note}</div>
+              <hr />
+              <div className='footer'>
+                <button onClick={() => deleteNote(note.id)}>Delete</button>
+                <button onClick={() => complete(note.id)}>Complete</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/contracts/Todo.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSE
+
+pragma solidity >=0.8.0;
+
+contract Todo {
+
+    event CreateNote(address indexed author, uint256 noteId);
+    event UpdateNote(address indexed author, uint256 noteId);
+    event DeleteNote(address indexed author, uint256 noteId);
+
+    struct Note {
+        uint256 noteId;
+        uint256 lastModified;
+        string title;
+        string note;
+        bool completed;
+        string tag;
+    }
+
+    mapping(address => Note[]) notes;
+    uint256 idController;
+
+    // add new note
+    function addNote(
+        string calldata _title, 
+        string calldata _note, 
+        string calldata _tag
+    ) external {
+        Note memory newNote = Note(idController, block.timestamp, _title, _note, false, _tag);
+        notes[msg.sender].push(newNote);
+        idController++;
+        emit CreateNote(msg.sender, idController);
+    }
+
+    // set a note to completed
+    function setCompleted(uint256 _noteId) external returns (bool) {
+        Note[] storage privateNotes = notes[msg.sender];
+        for (uint i = 0; i < privateNotes.length; i++) {
+            if (privateNotes[i].noteId == _noteId) {
+                privateNotes[i].completed = true;
+
+                notes[msg.sender] = privateNotes;
+                emit UpdateNote(msg.sender, _noteId);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // delete a note
+    function deleteNote(uint256 _noteId) external returns (bool) {
+        Note[] storage privateNotes = notes[msg.sender];
+        for (uint i = 0; i < privateNotes.length; i++) {
+            if (privateNotes[i].noteId == _noteId) {
+                privateNotes[i] = privateNotes[privateNotes.length - 1];
+                privateNotes.pop();                
+                
+                notes[msg.sender] = privateNotes;
+                emit DeleteNote(msg.sender, _noteId);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // get note belonging to message sender
+    function getNotes() external view returns (Note[] memory) {
+        Note[] memory _notes = notes[msg.sender];
+        return _notes;
+    }
+
+}
